369d9e1cbc156ec461330bead0b5fc276236b8e6
Mohammed Zaid <mohammedzee98@gmail.com>
2024-03-05 22:36:08 +0530

    session 1 completed

--- Lectures/FirstProg.py
+++ Lectures/FirstProg.py
@@ -1,15 +1,21 @@
 # print("Hello Zaidy")
 # print("You're dope bro")
 
-name = "Zaid"
-age =23
-price = "$546.78"
-print(price)
+# name = "Zaid"
+# age =23
+# price = "$546.78"
+# print(price)
 
-print("Greet me as", name)
-print(type(name))
+# print("Greet me as", name)
+# print(type(name))
 
-a = 3
-b = 6
-sum = a+b
-print(sum)  
+# a = 3
+# b = 6
+# sum = a+b
+# print(sum)
+
+A,B = "2",3
+txt = "@" 
+print((A+txt)*B)
+# Easier way to print the variables with the string we want.
+print(f"My name is {txt} and i am {A} years old")
