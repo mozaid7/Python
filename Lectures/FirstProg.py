d2b6fbf799f164cbbbd43a4675e092bed7c59c89
Mohammed Zaid <mohammedzee98@gmail.com>
2024-03-05 20:01:04 +0530

    basic types knowledge

--- Lectures/FirstProg.py
+++ Lectures/FirstProg.py
@@ -1,7 +1,10 @@
-print("Hello Zaidy")
-print("Youre dope bro")
+# print("Hello Zaidy")
+# print("Youre dope bro")
 
 name = "Zaid"
 age =23
 price = "$546.78"
 print(price)
+
+print("Greet me as", name)
+print(type(name))
