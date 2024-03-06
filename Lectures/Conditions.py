c756d6e4262026cf308d61dfd66468c781dfaacc
Mohammed Zaid <mohammedzee98@gmail.com>
2024-03-06 16:17:12 +0530

    strings

--- Lectures/Conditions.py
+++ Lectures/Conditions.py
@@ -1,6 +1,16 @@
-age = int(input("Enter age of the person: "))
-if (age>= 18) :
-    print("Eligible to vote.")
-else:
-    print("Not eligible to vote yet.")
+# age = int(input("Enter age of the person: "))
+# if (age>= 18) :
+#     print("Eligible to vote.")
+# else:
+#     print("Not eligible to vote yet.")
 
+# light = input("Light color:")
+# if(light == "Red"):
+#     print("Stop sign ahead!")
+# elif(light == "Green"):
+#     print("Go ahead!")
+# else:
+#     print("Caution, unknown light condition")
+
+str = "APPLE"
+print(str[:4]) #APPL #String Slicing
