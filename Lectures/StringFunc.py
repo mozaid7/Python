367a44a25f59a28a36a4bf313b38d886fe9ed54d
Mohammed Zaid <mohammedzee98@gmail.com>
2024-03-06 16:39:48 +0530

    Greatest Number function created

--- Lectures/StringFunc.py
+++ Lectures/StringFunc.py
@@ -1,19 +1,34 @@
-str = "I am a coder"
-print(str.endswith("er"))
+# str = "I am a coder"
+# print(str.endswith("er"))
 
-print(str.capitalize())
+# print(str.capitalize())
 
-print(str.count("a"))
+# print(str.count("a"))
 
-marks =56
+# marks =56
 
-if(marks>=90):
-    grade="A"
-elif(marks>=80 and marks<90):
-    grade = "B"
-elif(marks>=70 and marks<80):
-    grade = "C"
+# if(marks>=90):
+#     grade="A"
+# elif(marks>=80 and marks<90):
+#     grade = "B"
+# elif(marks>=70 and marks<80):
+#     grade = "C"
+# else:
+#     grade= "D"
+
+# print("Grade of the student is:", grade)
+
+#Greatest Number
+a = int(input("Enter a number: "))
+b = int(input("Enter a number: "))
+c = int(input("Enter a number: "))
+d = int(input("Enter a number: "))
+
+if(a>=b and a>=c):
+   print("The greatest number is: ", a)
+elif (b>=c and b>=d):
+    print("Second is the largest", b)
+elif (c>=d):
+    print("Third is the largest", c)
 else:
-    grade= "D"
-
-print("Grade of the student is:", grade)
+    print("Fourth is the largest", d)
