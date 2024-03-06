f55384f238e39f84f41a313a956ced9f93a89dd4
Mohammed Zaid <mohammedzee98@gmail.com>
2024-03-06 16:29:27 +0530

    marks func.

---
+++ Lectures/StringFunc.py
@@ -0,0 +1,19 @@
+str = "I am a coder"
+print(str.endswith("er"))
+
+print(str.capitalize())
+
+print(str.count("a"))
+
+marks =56
+
+if(marks>=90):
+    grade="A"
+elif(marks>=80 and marks<90):
+    grade = "B"
+elif(marks>=70 and marks<80):
+    grade = "C"
+else:
+    grade= "D"
+
+print("Grade of the student is:", grade)
